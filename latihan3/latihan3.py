f057771be5a59741df7ab9232dd86f9780d472fe
root <root@yanzz>
2022-11-07 23:27:24 +0700

    masih anget

---
+++ latihan3/latihan3.py
@@ -0,0 +1,11 @@
+print ("Jery Yanuarizy Sarjito")
+print ("20210801301")
+
+angka_pertama = int(input("Angka Pertama :"))
+Angka_kedua = int(input("Angka Kedua :"))
+
+jumlah = angka_pertama + Angka_kedua
+kurang = angka_pertama - Angka_kedua
+
+print ("Hasilnya Penjumlahan, ", jumlah)
+print ("Hasil Pengurangan, ", kurang)
